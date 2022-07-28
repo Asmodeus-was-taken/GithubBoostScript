3d8c89388bbf07420aed60243e914a0675e718c9
Asmodeuš <103351450+Asmodeus-was-taken@users.noreply.github.com>
2022-07-28 00:48:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ async function main () {
     var setTitle = require('console-title');
     setTitle(`Made by ImNotSummer | discord.gg/theolympus`);
     console.clear();
-    puppeteer.launch({ headless: true, executablePath: 'C://Program Files (x86)//Google//Chrome//Application//chrome.exe'}).then(async browser => { // change this to your chrome directory :)
+    puppeteer.launch({ headless: true, executablePath: 'C://Program Files (x86)//Google//Chrome//Application//chrome.exe'}).then(async browser => { 
         console.log('Starting Login')
         var isthere;
         const page = await browser.newPage()
         await page.setViewport({ width: 1920, height: 1080});
-        await page.goto('https://github.com/yourusername') //https://github.com/crackeddude
+        await page.goto('https://github.com/yourusername') 
         await sleep(3000)
 	browser.close();
 	main();
